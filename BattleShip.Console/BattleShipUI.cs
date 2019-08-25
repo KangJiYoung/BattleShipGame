683ba4ee360b9fd89f1cfa26fc43392a8acaa845
Teuca Sergiu Ionut <teucas@yahoo.com>
2019-08-26 00:56:05 +0300

    Refactor the UI

---
+++ BattleShip.Console/BattleShipUI.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Drawing;
+using BattleShip.GameLogic;
+
+namespace BattleShip
+{
+    public class BattleShipUI
+    {
+        private const int CONSOLE_CELL_WIDTH = 5;
+        private const int COORDINATES_LENGTH = 2;
+        private const int INDEX_OUTSIDE_OF_GRID = -1;
+        private const string TILE_UNKNOWN = "?";
+        private const string TILE_HIT = "X";
+        private const string TILE_MISS = "-";
+
+        public BattleShipGame BattleShipGame { get; }
+
+        public string Status { get; private set; }
+
+        public BattleShipUI(BattleShipGame battleShipGame)
+        {
+            BattleShipGame = battleShipGame;
+
+            Status = string.Empty;
+        }
+
+        public void Run()
+        {
+            BattleShipGame.NewGame();
+
+            while (!BattleShipGame.Grid.AreAllShipsDead)
+            {
+                for (var i = INDEX_OUTSIDE_OF_GRID; i < BattleShipGame.Grid.Rows; i++)
+                {
+                    for (var j = INDEX_OUTSIDE_OF_GRID; j < BattleShipGame.Grid.Columns; j++)
+                    {
+                        if (IsEmptyCell(i, j))
+                            DrawEmptyCell();
+                        else if (IsFirstColumn(i))
+                            DrawColumnNumbers(j);
+                        else if (IsFirstRow(j))
+                            DrawRowCharacters(i);
+                        else
+                            DrawGrid(i, j);
+                    }
+
+                    Console.WriteLine();
+                }
+
+                DisplayStatus();
+                HandleCoordinates();
+
+                Console.Clear();
+            }
+
+            DisplayEndingMessage();
+        }
+
+        private void DisplayEndingMessage()
+        {
+            Console.WriteLine("You Won!!");
+            DisplayScore();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        private void HandleCoordinates()
+        {
+            Console.Write("Enter coordinates: ");
+
+            var coordinates = Console.ReadLine();
+            if (coordinates.Length == COORDINATES_LENGTH)
+                Status = BattleShipGame.Hit(coordinates)
+                    ? "You hit a ship!"
+                    : "You missed!";
+            else
+                Status = "Invalid Coordinates";
+        }
+
+        private void DisplayStatus()
+        {
+            if (!string.IsNullOrWhiteSpace(Status))
+            {
+                Console.WriteLine(Status);
+                DisplayScore();
+            }
+        }
+
+        private void DisplayScore()
+        {
+            Console.WriteLine($"Score (number of hits): {BattleShipGame.Grid.Hits.Count}");
+        }
+
+        private void DrawGrid(int x, int y)
+        {
+            var point = new Point(x, y);
+            var isHit = BattleShipGame.Grid.Hits.ContainsKey(point);
+            if (isHit)
+                Console.Write(BattleShipGame.Grid.Hits[point]
+                    ? $"{TILE_HIT,CONSOLE_CELL_WIDTH}"
+                    : $"{TILE_MISS,CONSOLE_CELL_WIDTH}");
+            else
+                Console.Write($"{TILE_UNKNOWN,CONSOLE_CELL_WIDTH}");
+        }
+
+        private static void DrawRowCharacters(int row)
+        {
+            Console.Write($"{(char) ('A' + row),CONSOLE_CELL_WIDTH}");
+        }
+
+        private static bool IsFirstRow(int column) => column == INDEX_OUTSIDE_OF_GRID;
+
+        private static void DrawColumnNumbers(int number)
+        {
+            Console.Write($"{number,CONSOLE_CELL_WIDTH}");
+        }
+
+        private static bool IsFirstColumn(int columnIndex) => columnIndex == INDEX_OUTSIDE_OF_GRID;
+
+        private static void DrawEmptyCell()
+        {
+            Console.Write($"{string.Empty,CONSOLE_CELL_WIDTH}");
+        }
+
+        private static bool IsEmptyCell(int row, int column) =>
+            row == INDEX_OUTSIDE_OF_GRID && column == INDEX_OUTSIDE_OF_GRID;
+    }
+}
--- BattleShip.Console/Program.cs
+++ BattleShip.Console/Program.cs
@@ -1,69 +1,33 @@
 ﻿using System;
-using System.Drawing;
 using BattleShip.GameLogic;
 
 namespace BattleShip
 {
     internal static class Program
     {
-        private const int CONSOLE_CELL_WIDTH = 5;
+        private const int GRID_ROWS = 10;
+        private const int GRID_COLUMNS = 10;
+        private const int BATTLESHIP_SIZE = 5;
+        private const int DESTROYER_SIZE = 4;
 
         private static void Main()
         {
-            var grid = new Grid(10, 10);
-            var randomGenerator = new RandomGenerator();
-            var battleShipGame = new BattleShipGame(grid, randomGenerator);
-            battleShipGame.NewGame();
-
-            var status = string.Empty;
-            
-            while (!battleShipGame.Grid.AreAllShipsDead)
+            try
             {
-                for (var i = -1; i < battleShipGame.Grid.Rows; i++)
-                {
-                    for (var j = -1; j < battleShipGame.Grid.Columns; j++)
-                    {
-                        if (i == -1 && j == -1)
-                            Console.Write($"{string.Empty,CONSOLE_CELL_WIDTH}");
-                        else if (i == -1)
-                            Console.Write($"{j,CONSOLE_CELL_WIDTH}");
-                        else if (j == -1)
-                            Console.Write($"{(char) ('A' + i),CONSOLE_CELL_WIDTH}");
-                        else
-                        {
-                            var point = new Point(i, j);
-                            var isHit = battleShipGame.Grid.Hits.ContainsKey(point);
-                            if (!isHit)
-                                Console.Write($"{"?",CONSOLE_CELL_WIDTH}");
-                            else
-                                Console.Write(battleShipGame.Grid.Hits[point]
-                                    ? $"{"X",CONSOLE_CELL_WIDTH}"
-                                    : $"{"-",CONSOLE_CELL_WIDTH}");
-                        }
-                    }
-
-                    Console.WriteLine();
-                }
-
-                if (!string.IsNullOrWhiteSpace(status))
-                {
-                    Console.WriteLine(status);
-                    Console.WriteLine($"Score (number of hits): {battleShipGame.Grid.Hits.Count}");
-                }
+                var grid = new Grid(GRID_ROWS, GRID_COLUMNS);
+                var randomGenerator = new RandomGenerator();
+                var battleShipGame = new BattleShipGame(grid, randomGenerator);
+                battleShipGame.AddShip(new Ship(BATTLESHIP_SIZE, ShipType.BattleShip));
+                battleShipGame.AddShip(new Ship(DESTROYER_SIZE, ShipType.Destroyer));
+                battleShipGame.AddShip(new Ship(DESTROYER_SIZE, ShipType.Destroyer));
                 
-                Console.Write("Enter coordinates: ");
-                var coordinates = Console.ReadLine();
-                if (coordinates.Length == 2)
-                    status = battleShipGame.Hit(coordinates)
-                        ? "You hit a ship!"
-                        : "You missed!";
-                else
-                    status = "Invalid Coordinates";
+                var battleShipUI = new BattleShipUI(battleShipGame);
+                battleShipUI.Run();
             }
-
-            Console.WriteLine("You Won!!");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadLine();
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
